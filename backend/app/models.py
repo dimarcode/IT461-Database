c041c18cea424b7032dbe8b105941120afffd55e
dimarcode <codydimarcolr@gmail.com>
2025-03-08 22:55:32 -0500

    push to main before new branch

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,6 +9,16 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+# !!!!! any changes to this file require a db migration to take effect!!!!!:
+# first enter virtual environment (example uses docker):
+# $ docker exec -it <container-name> sh
+# >>> flask db migrate -m "<context for migration>"
+# >>> flask db upgrade
+# to rollback changes for whatever reason:
+# >>> flask db downgrade
+# url for migration details: https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-iv-database
+
+
 @login.user_loader
 def load_user(id):
     return db.session.get(User, int(id))
@@ -20,40 +30,34 @@ class User(UserMixin, db.Model):
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
                                              unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    # links to post entry
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(
-        back_populates='author')
-    
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
-    
     last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
         default=lambda: datetime.now(timezone.utc))
-    
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
-
+    # links to post #
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
     def __repr__(self):
         return '<User {}>'.format(self.username)
-
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
-
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 
 class Post(db.Model):
+    # primary key
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    # foreign key from 'user' table
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
+                                               index=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(
         index=True, default=lambda: datetime.now(timezone.utc))
-    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id),
-                                               index=True)
     # links to user entry
     author: so.Mapped[User] = so.relationship(back_populates='posts')
-
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
